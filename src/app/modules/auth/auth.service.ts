4446336203d8c5967328d14775beca5de29fffeb
Lucbm99 <lbaradelmarchiori@gmail.com>
2022-11-21 22:31:19 -0200

    Fix - serviço de autenticação

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseURL: string = 'http://localhost:4000/';
+
+  constructor(
+    private _httpClient: HttpClient
+  ) { }
+
+  public getUsuarios(): Observable<any> {
+    return this._httpClient.get<any>(`${this.baseURL}usuarios`)
+    .pipe(
+      tap((response: any) => 
+        response
+    ),  
+    catchError((error: HttpErrorResponse) => {
+      console.log('error', error)
+      return EMPTY;
+    }),
+  )}
+
+  public fazerLogin() {
+
+  }
+
+  public fazerCadastro() {
+
+  }
+
+  public esqueciSenha() {
+    
+  }
 }
